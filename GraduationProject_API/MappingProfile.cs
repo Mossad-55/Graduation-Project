9c7081585a1eaf50b8d56bdb0e32c1a782bde912
Mossad <mosad55522@gmail.com>
2023-03-10 18:13:27 +0200

    Added MappingProfile.cs For AutoMapper

---
+++ GraduationProject_API/MappingProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace GraduationProject_API;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+        // All the mapping will be added later.
+    }
+}
