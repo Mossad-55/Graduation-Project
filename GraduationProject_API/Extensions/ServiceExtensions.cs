c157cc0589b944df2ed7b2bbcae8f8231cb127bf
Mossad <mosad55522@gmail.com>
2023-03-10 18:11:31 +0200

    Added ServiceExtensions.cs with Services Configuration methods

---
+++ GraduationProject_API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Contracts;
+using LoggerService;
+using Microsoft.EntityFrameworkCore;
+using Repository;
+
+namespace GraduationProject_API.Extensions;
+
+public static class ServiceExtensions
+{
+    // CORS Configuration.
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+        });
+
+    // IIS Configuration. (Default)
+    public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+            // Multiple Options will be added later.
+        });
+
+    // Configure Logger Services.
+    public static void ConfigureLoggerServices(this IServiceCollection services) =>
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+
+    // Configure the RepositoryContext at runtime.
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+}
