117e920b204b618b4873391d6cf87b9635a12794
Mossad <mosad55522@gmail.com>
2023-03-10 17:50:43 +0200

    Adding ContextFactory Folder with RepositoryContextFactory.cs

---
+++ GraduationProject_API/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace GraduationProject_API.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlServer(configuration.GetConnectionString("sqlConnection"),
+            b => b.MigrationsAssembly("GraduationProject_API"));
+
+        return new RepositoryContext(builder.Options);
+    }
+}
