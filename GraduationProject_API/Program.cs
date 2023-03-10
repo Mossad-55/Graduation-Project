cfa10e1c716f2f70d0fa2f9a17106b5c5fa3fed2
Mossad <mosad55522@gmail.com>
2023-03-10 18:28:13 +0200

    Edited the Program.cs File.

--- GraduationProject_API/Program.cs
+++ GraduationProject_API/Program.cs
@@ -1,13 +1,54 @@
+using Contracts;
+using GraduationProject_API.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Logger File Configuration.
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerServices();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+
+// Configure AutoMapper Profile.
+builder.Services.AddAutoMapper(typeof(Program));
+
+// Changing the default behavior of the controllers.
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+// Configuring the controllers to be in the Presentation layer as well as the formats.
+builder.Services.AddControllers(config =>
+{
+    config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
+}).AddApplicationPart(typeof(GraduationProject_API.Presentation.AssemblyReference).Assembly);
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuring Exception Handler.
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
+if (app.Environment.IsProduction())
+    app.UseHsts();
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
