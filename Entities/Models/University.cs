238c24ed3dd9bbf42a9fabd459d8251eab15c18a
Mossad <mosad55522@gmail.com>
2023-03-18 12:41:55 +0200

    Added University.cs Entity

---
+++ Entities/Models/University.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class University
+{
+    [Column]
+    public Guid Id { get; set; }
+    
+    [Required(ErrorMessage = "University name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the University name is 60 characters.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "University description is a required field.")]
+    public string? Description { get; set; }
+
+}
