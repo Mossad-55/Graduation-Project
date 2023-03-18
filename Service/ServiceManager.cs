db61fa58e421920827c3f1e4a8eef79a297871f5
Mossad <mosad55522@gmail.com>
2023-03-18 13:27:17 +0200

    Implemented IServiceManager.cs Interface

---
+++ Service/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+public sealed class ServiceManager : IServiceManager
+{
+    private readonly Lazy<IUniversityService> _universityService;
+
+	public ServiceManager(IRepositoryManager repositoryManager, 
+		ILoggerManager logger, IMapper mapper)
+	{
+		_universityService = new Lazy<IUniversityService>(() => new UniversityService(repositoryManager, logger, mapper));
+	}
+
+	public IUniversityService UniversityService => _universityService.Value;
+}
