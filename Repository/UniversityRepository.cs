542a9e212ef0bd0574221e450f98ebb8f0e5d70e
Mossad <mosad55522@gmail.com>
2023-03-18 12:51:57 +0200

    Implemented IUniversityRepository.cs Interface

---
+++ Repository/UniversityRepository.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository;
+
+internal sealed class UniversityRepository : RepositoryBase<University>, IUniversityRepository
+{
+    
+    public UniversityRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+    {
+    }
+
+    public IEnumerable<University> GetAllUniversities(bool trackChanges) =>
+        FindAll(trackChanges)
+        .OrderBy(u => u.Name)
+        .ToList();
+
+    public University GetUniversity(Guid id, bool trackChanges) =>
+        FindByCondition(u => u.Id == id, trackChanges)
+        .SingleOrDefault();
+
+    public void CreateUniversity(University university) => Create(university);
+
+    public void DeleteUniversity(University university) => Delete(university);
+}
