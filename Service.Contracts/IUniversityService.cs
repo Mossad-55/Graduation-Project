27ea3a3c9fa6d9eed5b195ed2872a94bd708062a
Mossad <mosad55522@gmail.com>
2023-03-18 13:09:20 +0200

    Created IUniversityService.cs Interface

---
+++ Service.Contracts/IUniversityService.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Models;
+
+namespace Service.Contracts;
+
+public interface IUniversityService
+{
+    IEnumerable<University> GetAllUniversities(bool trackChanges);
+    University GetUniversity(Guid id, bool trackChanges);
+
+}
