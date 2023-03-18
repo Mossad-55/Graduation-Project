df6d5a814c221b9afc21ff38f5b5c578a337d8ae
Mossad <mosad55522@gmail.com>
2023-03-18 12:58:42 +0200

    Implemeneted IRepositoryManager.cs Interface

---
+++ Repository/RepositoryManager.cs
@@ -0,0 +1,20 @@
+﻿using Contracts;
+
+namespace Repository;
+
+public sealed class RepositoryManager : IRepositoryManager
+{
+    private readonly RepositoryContext _repositoryContext;
+    private readonly Lazy<IUniversityRepository> _universityRepository;
+
+    public RepositoryManager(RepositoryContext repositoryContext)
+    {
+        _repositoryContext = repositoryContext;
+        _universityRepository = new Lazy<IUniversityRepository>(() => new UniversityRepository(repositoryContext));
+    }
+
+    public IUniversityRepository University => _universityRepository.Value;
+
+
+    public void Save() => _repositoryContext.SaveChanges();
+}
