8d699ddbe7677b37ed31971057989dd1775d0b3c
Mossad <mosad55522@gmail.com>
2023-03-18 13:21:38 +0200

    Added UniversityNotFoundException.cs Class

---
+++ Entities/Exceptions/UniversityNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class UniversityNotFoundException : NotFoundException
+{
+    public UniversityNotFoundException(Guid id) 
+        : base($"The university with id: {id} doesn't exists in the database.")
+    {
+    }
+}
