c638de8bb78377c2d933648d6f9d84f00f394813
Mossad <mosad55522@gmail.com>
2023-03-18 12:48:02 +0200

    Added IUniversityRepository Interface

---
+++ Contracts/IUniversityRepository.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Models;
+
+namespace Contracts;
+
+public interface IUniversityRepository
+{
+    IEnumerable<University> GetAllUniversities(bool trackChanges);
+    University GetUniversity(Guid id, bool trackChanges);
+    void CreateUniversity(University university);
+    void DeleteUniversity(University university);
+}
