e9260d1d02685b86dc213480951c1838bd8cfa71
Mossad <mosad55522@gmail.com>
2023-03-18 13:19:36 +0200

    Implemented IUniversityService.cs Interface

---
+++ Service/UniversityService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Contracts;
+using Service.Contracts;
+using Shared.DataTranferObjects;
+using System.Diagnostics;
+
+namespace Service;
+
+internal sealed class UniversityService : IUniversityService
+{
+    private readonly IRepositoryManager _repository;
+    private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
+
+    public UniversityService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+    {
+        _repository = repository;
+        _logger = logger;
+        _mapper = mapper;
+    }
+
+    public UniversityDto CreateUniversity(UniversityForCreationDto university)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<UniversityDto> GetAllUniversities(bool trackChanges)
+    {
+        var universities = _repository.University.GetAllUniversities(trackChanges);
+
+        var universitiesDto = _mapper.Map<IEnumerable<UniversityDto>>(universities);
+
+        return universitiesDto;
+    }
+
+    public UniversityDto GetUniversity(Guid id, bool trackChanges)
+    {
+        var university = _repository.University.GetUniversity(id, trackChanges);
+
+        if (university is null)
+            throw new UniversityNotFoundException(id);
+
+        var universityDto = _mapper.Map<UniversityDto>(university);
+
+        return universityDto;
+    }
+}
