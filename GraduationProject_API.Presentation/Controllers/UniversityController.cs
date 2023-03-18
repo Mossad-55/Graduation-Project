98010e7e837af8bacb3b82e92406b78524434da2
Mossad <mosad55522@gmail.com>
2023-03-18 13:36:36 +0200

    Created the University Controller with single endpoint

---
+++ GraduationProject_API.Presentation/Controllers/UniversityController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+
+namespace GraduationProject_API.Presentation.Controllers;
+
+[Route("api/universities")]
+[ApiController]
+public class UniversityController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public UniversityController(IServiceManager service) => _service = service;
+
+    [HttpGet]
+    public IActionResult GetUniversities()
+    {
+        var universities = _service.UniversityService.GetAllUniversities(trackChanges: false);
+        return Ok(universities);
+    }
+    
+}
