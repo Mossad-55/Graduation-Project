11f119f9a26794b29b3ad60f58e3bafb8d17c283
Mossad <mosad55522@gmail.com>
2023-03-23 21:37:02 +0200

    Added IFacultyRepository.cs Inferface and its implementation

---
+++ Contracts/IFacultyRepository.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Models;
+
+namespace Contracts;
+
+public interface IFacultyRepository
+{
+    IEnumerable<Faculty> GetAllFaculitites(Guid universityId, bool trackChanges);
+    Faculty GetFaculty(Guid universityId, Guid id, bool trackChanges);
+    void CreateFacultyForUniversity(Guid universityId, Faculty faculty);
+    void DeleteFaculty(Faculty faculty);
+}
---
+++ Repository/FacultyRepository.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository;
+
+internal sealed class FacultyRepository : RepositoryBase<Faculty>, IFacultyRepository
+{
+    public FacultyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+    {
+    }
+
+    public void CreateFacultyForUniversity(Guid universityId, Faculty faculty)
+    {
+        faculty.UniversityId = universityId;
+        Create(faculty);
+    }
+
+    public void DeleteFaculty(Faculty faculty) => Delete(faculty);
+
+    public IEnumerable<Faculty> GetAllFaculitites(Guid universityId, bool trackChanges) =>
+        FindByCondition(f => f.UniversityId == universityId, trackChanges)
+        .OrderBy(f => f.Name)
+        .ToList();
+
+    public Faculty GetFaculty(Guid universityId, Guid id, bool trackChange) =>
+        FindByCondition(f => f.UniversityId == universityId && f.Id == id, trackChange)
+        .SingleOrDefault();
+}
