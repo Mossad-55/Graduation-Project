16ae652604e158bbb96000630183a01c86929776
Mossad <mosad55522@gmail.com>
2023-03-25 00:54:17 +0200

    Added Department.cs Entity

---
+++ Entities/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
+
+public class Department
+{
+    [Column("DepartmentId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Department name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "Description is a required field.")]
+    public string? Description { get; set; }
+    public double Rate { get; set; }
+
+    [ForeignKey(nameof(Faculty))]
+    public Guid FacultyId { get; set; }
+    public Faculty? Faculty { get; set; }
+}
