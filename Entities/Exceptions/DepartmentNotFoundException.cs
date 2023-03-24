e204e32b0c66367de65510a39d32ee377d1b7b45
Mossad <mosad55522@gmail.com>
2023-03-25 01:23:07 +0200

    Added NotFound Exception Model For Department

---
+++ Entities/Exceptions/DepartmentNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class DepartmentNotFoundException : NotFoundException
+{
+    public DepartmentNotFoundException(Guid id)
+        : base($"The department with id: {id} doesn't exists in the database.")
+{
+}
+}
