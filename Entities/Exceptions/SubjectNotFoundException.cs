ef06b9882938637ebe0f79a02562844e90411a2e
Mossad <mosad55522@gmail.com>
2023-03-26 20:43:27 +0200

    Added Not Found Exception For Subject

---
+++ Entities/Exceptions/SubjectNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class SubjectNotFoundException : NotFoundException
+{
+    public SubjectNotFoundException(Guid id)
+        : base($"The subject with id: {id} doesn't exists in the database.")
+    {
+    }
+}
