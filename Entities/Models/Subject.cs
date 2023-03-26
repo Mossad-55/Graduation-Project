8c9e45c7cb986efb98b483de75527cb758437c45
Mossad <mosad55522@gmail.com>
2023-03-26 20:40:29 +0200

    Added Subject Entity

---
+++ Entities/Models/Subject.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models;
+
+public class Subject
+{
+    [Column("SubjectId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Subject name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string? Name { get; set; }
+    [Required(ErrorMessage = "Subject code field is a required field.")]
+    public string? Code { get; set; }
+
+    [Required(ErrorMessage = "Description is a required field.")]
+    public string? Description { get; set; }
+    public double Rate { get; set; }
+
+    [ForeignKey(nameof(Department))]
+    public Guid DepartmentId { get; set; }
+    public Department? Department { get; set; }
+}
