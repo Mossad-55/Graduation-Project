cac0596876657ada1a7db7519f41e286b1ce262f
Mossad <mosad55522@gmail.com>
2023-03-27 20:31:39 +0200

    Configuring ISubjectRepository.cs Interface in IRepositoryManager.cs

--- Contracts/IRepositoryManager.cs
+++ Contracts/IRepositoryManager.cs
@@ -6,6 +6,7 @@ public interface IRepositoryManager
     IUniversityRepository University { get; }
     IFacultyRepository Faculty { get; }
     IDepartmentRepository Department { get; }
+    ISubjectRepository Subject { get; }
 
     // Shared Functions.
     void Save();
--- Repository/RepositoryManager.cs
+++ Repository/RepositoryManager.cs
@@ -8,8 +8,7 @@ public sealed class RepositoryManager : IRepositoryManager
     private readonly Lazy<IUniversityRepository> _universityRepository;
     private readonly Lazy<IFacultyRepository> _facultyRepository;
     private readonly Lazy<IDepartmentRepository> _departmentRepository;
-    
-
+    private readonly Lazy<ISubjectRepository> _subjectRepository;
 
     public RepositoryManager(RepositoryContext repositoryContext)
     {
@@ -17,11 +16,13 @@ public sealed class RepositoryManager : IRepositoryManager
         _universityRepository = new Lazy<IUniversityRepository>(() => new UniversityRepository(repositoryContext));
         _facultyRepository = new Lazy<IFacultyRepository>(() => new FacultyRepository(repositoryContext));
         _departmentRepository = new Lazy<IDepartmentRepository>(() => new DepartmentRepository(repositoryContext));
+        _subjectRepository = new Lazy<ISubjectRepository>(() => new SubjectRepository(repositoryContext));
     }
 
     public IUniversityRepository University => _universityRepository.Value;
     public IFacultyRepository Faculty => _facultyRepository.Value;
     public IDepartmentRepository Department => _departmentRepository.Value;
+    public ISubjectRepository Subject => _subjectRepository.Value;
 
 
     public void Save() => _repositoryContext.SaveChanges();
