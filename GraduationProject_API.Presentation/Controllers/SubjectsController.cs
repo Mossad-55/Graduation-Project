87b6d93646ebe20062a71a4f150a2a1ec27b959e
Mossad <mosad55522@gmail.com>
2023-04-03 21:44:34 +0200

    Edited The SubjectsController.cs

--- GraduationProject_API.Presentation/Controllers/SubjectsController.cs
+++ GraduationProject_API.Presentation/Controllers/SubjectsController.cs
@@ -5,7 +5,7 @@ using Shared.DataTranferObjects;
 namespace GraduationProject_API.Presentation.Controllers;
 
 [ApiController]
-[Route("api/universities/{universityId}/faculities/{facultyId}/departments/{departmentId}/subjects")]
+[Route("api/faculities/{facultyId}/departments/{departmentId}/subjects")]
 public class SubjectsController : ControllerBase
 {
     private readonly IServiceManager _service;
@@ -13,48 +13,48 @@ public class SubjectsController : ControllerBase
 	public SubjectsController(IServiceManager service) => _service = service;
 
     [HttpGet]
-    public IActionResult GetSubjectsForDepartment(Guid universityId, Guid facultyId, Guid departmentId)
+    public IActionResult GetSubjectsForDepartment(Guid facultyId, Guid departmentId)
     {
-        var subjects = _service.SubjectService.GetAllSubjects(universityId, facultyId, departmentId ,false);
+        var subjects = _service.SubjectService.GetAllSubjects(facultyId, departmentId ,false);
 
         return Ok(subjects);
     }
 
     [HttpGet("{id:Guid}", Name = "GetSubjectForDepartment")]
-    public IActionResult GetSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid id)
+    public IActionResult GetSubject(Guid facultyId, Guid departmentId, Guid id)
     {
-        var subject = _service.SubjectService.GetSubject(universityId, facultyId, departmentId, id, false);
+        var subject = _service.SubjectService.GetSubject(facultyId, departmentId, id, false);
 
         return Ok(subject);
     }
 
     [HttpPost]
-    public IActionResult CreateSubject(Guid universityId, Guid facultyId, Guid departmentId, [FromBody] SubjectForCreationDto subject)
+    public IActionResult CreateSubject(Guid facultyId, Guid departmentId, [FromBody] SubjectForCreationDto subject)
     {
         if (subject is null)
             return BadRequest("Object is null");
 
-        var subjectToReturn = _service.SubjectService.CreateSubjectForDepartment(universityId, facultyId, departmentId, subject, false);
+        var subjectToReturn = _service.SubjectService.CreateSubjectForDepartment(facultyId, departmentId, subject, false);
 
-        return CreatedAtRoute("GetSubjectForDepartment", new { universityId, facultyId, departmentId, id = subjectToReturn.Id }, subjectToReturn);
+        return CreatedAtRoute("GetSubjectForDepartment", new { facultyId, departmentId, id = subjectToReturn.Id }, subjectToReturn);
     }
 
     [HttpDelete("{id:Guid}")]
-    public IActionResult DeleteSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid id)
+    public IActionResult DeleteSubject(Guid facultyId, Guid departmentId, Guid id)
     {
-        _service.SubjectService.DeleteSubjectForDepartment(universityId, facultyId, departmentId, id, false);
+        _service.SubjectService.DeleteSubjectForDepartment(facultyId, departmentId, id, false);
 
         return NoContent();
     }
 
     [HttpPut("{id:Guid}")]
-    public IActionResult UpdateSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid id,
+    public IActionResult UpdateSubject(Guid facultyId, Guid departmentId, Guid id,
         [FromBody] SubjectForUpdateDto subject)
     {
         if (subject is null)
             return BadRequest("Object is null");
 
-        _service.SubjectService.UpdateSubjectForDepartment(universityId, facultyId, departmentId, id, subject, false, true);
+        _service.SubjectService.UpdateSubjectForDepartment(facultyId, departmentId, id, subject, false, true);
 
         return NoContent();
     }
