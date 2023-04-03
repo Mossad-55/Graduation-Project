2959343a7af2acec433204012a6d055ef20a9521
Mossad <mosad55522@gmail.com>
2023-04-03 21:12:13 +0200

    Created IQuestionnaireService Interface and Implementing it

---
+++ Service.Contracts/IQuestionnaireService.cs
@@ -0,0 +1,12 @@
+﻿using Shared.DataTranferObjects;
+
+namespace Service.Contracts;
+
+public interface IQuestionnaireService
+{
+    IEnumerable<QuestionnaireDto> GetAllQuestionnaires(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, bool trackChanges);
+    QuestionnaireDto GetQuestionnaire(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, Guid id, bool trackChanges);
+    QuestionnaireDto CreateQuestionnaireForSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, QuestionnaireForCreateDto questionnaire, bool trackChanges);
+    void DeleteQuestionnaireForSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, Guid id, bool trackChanges);
+    void UpdateQuestionnaireForSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, Guid id, QuestionnaireForUpdateDto questionnaire, bool otherTrackChanges, bool quesTrackChanges);
+}
---
+++ Service/QuestionnaireService.cs
@@ -0,0 +1,32 @@
+﻿using Service.Contracts;
+using Shared.DataTranferObjects;
+
+namespace Service;
+
+internal sealed class QuestionnaireService : IQuestionnaireService
+{
+    public QuestionnaireDto CreateQuestionnaireForSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, QuestionnaireForCreateDto questionnaire, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteQuestionnaireForSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, Guid id, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<QuestionnaireDto> GetAllQuestionnaires(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public QuestionnaireDto GetQuestionnaire(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, Guid id, bool trackChanges)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateQuestionnaireForSubject(Guid universityId, Guid facultyId, Guid departmentId, Guid subjectId, Guid id, QuestionnaireForUpdateDto questionnaire, bool otherTrackChanges, bool quesTrackChanges)
+    {
+        throw new NotImplementedException();
+    }
+}
