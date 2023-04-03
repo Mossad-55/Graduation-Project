091dc6997e115fd565190eb048389ab5c7e2a77e
Mossad <mosad55522@gmail.com>
2023-04-03 21:13:35 +0200

    Added QuestionnaireNotFoundException.cs Class

---
+++ Entities/Exceptions/QuestionnaireNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Exceptions;
+
+public sealed class QuestionnaireNotFoundException : NotFoundException
+{
+    public QuestionnaireNotFoundException(Guid id)
+        : base($"The questionnaire with id: {id} doesn't exists in the database.")
+    {
+    }
+}
