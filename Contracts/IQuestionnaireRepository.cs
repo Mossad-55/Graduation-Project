f427584925ce1b0e944f21f6d1e1b30cac5fef55
Mossad <mosad55522@gmail.com>
2023-04-03 21:04:02 +0200

    Added IQuestionnaireRepository Interface and Its Implementation

---
+++ Contracts/IQuestionnaireRepository.cs
@@ -0,0 +1,11 @@
+﻿using Entities.Models;
+
+namespace Contracts;
+
+public interface IQuestionnaireRepository
+{
+    IEnumerable<Questionnaire> GetAllQuestionnaires(Guid subjectId, bool trackChanges);
+    Questionnaire GetQuestionnaire(Guid subjectId, Guid id, bool trackChanges);
+    void CreateQuestionnaire(Guid subjectId, Questionnaire questionnaire);
+    void DeleteQuestionnaire(Questionnaire questionnaire);
+}
---
+++ Repository/QuestionnaireRepository.cs
@@ -0,0 +1,28 @@
+﻿using Contracts;
+using Entities.Models;
+
+namespace Repository;
+
+internal sealed class QuestionnaireRepository : RepositoryBase<Questionnaire>, IQuestionnaireRepository
+{
+    public QuestionnaireRepository(RepositoryContext repositoryContext) : base(repositoryContext)
+    {
+    }
+
+    public void CreateQuestionnaire(Guid subjectId, Questionnaire questionnaire)
+    {
+        questionnaire.SubjectId = subjectId;
+        Create(questionnaire);
+    }
+
+    public void DeleteQuestionnaire(Questionnaire questionnaire) => Delete(questionnaire);
+
+    public IEnumerable<Questionnaire> GetAllQuestionnaires(Guid subjectId, bool trackChanges) =>
+        FindByCondition(q => q.SubjectId == subjectId, trackChanges)
+        .OrderBy(q => q.CreatedAt)
+        .ToList();
+
+    public Questionnaire GetQuestionnaire(Guid subjectId, Guid id, bool trackChanges) =>
+        FindByCondition(q => q.SubjectId == subjectId && q.Id == id, trackChanges)
+        .FirstOrDefault();
+}
