8f9f4ec97988f1d6c53cd69c890fc5216ce098cd
Mossad <mosad55522@gmail.com>
2023-04-04 03:24:10 +0200

    Added RolesConfiguration.cs Class

---
+++ Repository/Configuration/RoleConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Configuration;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+    public void Configure(EntityTypeBuilder<IdentityRole> builder)
+    {
+        builder.HasData
+        (
+            new IdentityRole
+            {
+                Name = "University Admin",
+                NormalizedName = "UNIVERSITY ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "Faculty Admin",
+                NormalizedName = "FACULTY ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "Department Admin",
+                NormalizedName = "DEPARTMENT ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "Professor",
+                NormalizedName = "PROFESSOR"
+            },
+            new IdentityRole
+            {
+                Name = "Student",
+                NormalizedName = "STUDENT"
+            }
+        );
+    }
+}
