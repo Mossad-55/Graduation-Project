638f27e0b8be324fdcabcd76c672fb2e681b12d1
Mossad <mosad55522@gmail.com>
2023-04-04 02:54:07 +0200

    Implemented Identity

---
+++ Entities/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    [ForeignKey(nameof(University))]
+    public Guid UniversityId { get; set; }
+    public University? University { get; set; }
+}
--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,10 +1,11 @@
 ﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Repository.Configuration;
 
 namespace Repository;
 
-public class RepositoryContext : DbContext
+public class RepositoryContext : IdentityDbContext<User>
 {
 	public RepositoryContext(DbContextOptions options)
 		: base(options)
@@ -14,6 +15,8 @@ public class RepositoryContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         // Applying Configuration For Each Entity.
         modelBuilder.ApplyConfiguration(new UniversityConfiguration());
         modelBuilder.ApplyConfiguration(new FacultyConfiguration());
