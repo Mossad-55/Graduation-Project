43416f308eefaa8e49297f2550966bdde7bb4689
Mossad <mosad55522@gmail.com>
2023-04-05 20:20:03 +0200

    Added UniversityAdmin.cs Entity in Models Folder

---
+++ Entities/Models/UniversityAdmin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class UniversityAdmin
+{
+    public Guid Id { get; set; }
+    public string? FullName { get; set; }
+
+    [ForeignKey(nameof(University))]
+    public Guid UniversityId { get; set; }
+    public University? University { get; set; }
+}
