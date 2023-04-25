68672c29f835db96ee0cafe491cbcbbb5443a3e5
Mossad <mosad55522@gmail.com>
2023-04-25 13:05:30 +0200

    Added More Configuration Files

--- Repository/Configuration/FacultyAdminConfiguration.cs
+++ Repository/Configuration/FacultyAdminConfiguration.cs
@@ -12,9 +12,9 @@ public class FacultyAdminConfiguration : IEntityTypeConfiguration<FacultyAdmin>
         (
             new FacultyAdmin
             {
-                Id = new Guid(""),
-                FacultyId = new Guid(""),
-                UniveristyId = new Guid(""),
+                Id = new Guid("69E0E900-6DE2-45E8-85CA-583B32C5C5AA"),
+                FacultyId = new Guid("D0552B49-6E7D-4CED-8A30-62CE8066A2D4"),
+                UniveristyId = new Guid("86f697d4-a762-44d6-8322-2c08c66f94e4"),
             }
         );
     }
--- Repository/Configuration/ProfessorConfiguration.cs
+++ Repository/Configuration/ProfessorConfiguration.cs
@@ -4,15 +4,15 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Repository.Configuration;
 
-public class DepartmentAdminConfiguration : IEntityTypeConfiguration<DepartmentAdmin>
+public class ProfessorConfiguration : IEntityTypeConfiguration<Professor>
 {
-    public void Configure(EntityTypeBuilder<DepartmentAdmin> builder)
+    public void Configure(EntityTypeBuilder<Professor> builder)
     {
         builder.HasData
         (
-            new DepartmentAdmin
+            new Professor
             {
-                Id = new Guid("7A369173-1E2F-491F-874F-7B324C034BC2"),
+                Id = new Guid("FDC74EF3-1E3D-4A13-9F19-4E381CE4C3D2"),
                 DepartmentId = new Guid("84796C48-D538-4954-A98A-622DC5C9325A"),
                 FacultyId = new Guid("D0552B49-6E7D-4CED-8A30-62CE8066A2D4"),
                 UniveristyId = new Guid("86f697d4-a762-44d6-8322-2c08c66f94e4")
